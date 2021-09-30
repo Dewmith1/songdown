fae61fad9f7b9d05497d5c03749c73da599107a4
CGS-TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-09-30 09:38:05 +0530

    Update __main__.py

--- SDSongBot/__main__.py
+++ SDSongBot/__main__.py
@@ -21,13 +21,13 @@
 #SOFTWARE.
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from bot import bot as app
-from bot import LOGGER
+from SDSongBot import bot as app
+from SDSongBot import LOGGER
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied #fsub
-from bot.plugins import *
+from SDSongBot.plugins import *
 from pyrogram import idle, filters
-from bot.plugins.Dev import *
+from SDSongBot.plugins.Dev import *
 from config import BOT_USERNAME
 
 JOIN_ASAP = " **You cant use me untill subscribe our updates channel** ☹️\n\n So Please join our updates channel by the following button and hit on the ` /start ` button again 😊"
